a8be76ebcaddc60d326c4090a2788d9aecd5b977
Tom MacWright <tom@macwright.org>
2014-06-12 09:34:43 -0400

    Fix style issues

--- index.js
+++ index.js
@@ -5,39 +5,37 @@ var findit = require('findit'),
 
 var finder = findit(argv._[0] || '.');
 var prefix = argv.prefix;
-if (prefix == undefined) {
-	process.stdout.write('Error: Missing Prefix.\n')
-	process.stdout.write('  Eg: sitemap-static --prefix=http://www.domain.com\n')
-	process.exit()
+if (prefix === undefined) {
+    process.stdout.write('Error: Missing Prefix.\n')
+    process.stdout.write('  Eg: sitemap-static --prefix=http://www.domain.com\n')
+    process.exit()
 }
 
-// add the trailing slash to the prefix if it's not there.
-if (prefix.substr(prefix.length-1) != '/')
-	prefix += '/'
+if (!prefix.match(/\/$/)) prefix += '/'
 
 var ignore_file = argv['ignore-file'];
 var ignore = []
 var ignore_folders = []
 
 if (ignore_file) {
-	ignore = require(process.cwd() + '/' + ignore_file);
-	var len = ignore.length
-	for (var i = 0; i < len; i++) {
-		var l = ignore[i].length
-		if (ignore[i].substr(l-5) !== '.html') {
-			ignore_folders.push(new RegExp('^' + ignore[i]))
-		}
-	}
+    ignore = require(process.cwd() + '/' + ignore_file);
+    var len = ignore.length
+    for (var i = 0; i < len; i++) {
+        var l = ignore[i].length
+        if (ignore[i].substr(l-5) !== '.html') {
+            ignore_folders.push(new RegExp('^' + ignore[i]))
+        }
+    }
 }
 
 process.stdout.write('<?xml version="1.0" encoding="UTF-8"?>');
 process.stdout.write('<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">');
 
 finder.on('file', function(file, stat) {
-    if (file.indexOf('.html') === -1) return;
-    if (ignore.indexOf(file) !== -1) return;
-    for (var i =0; i<ignore_folders.length; i++) {
-    	if (file.match(ignore_folders[i])) return;
+    if (file.indexOf('.html') === -1 ||
+        ignore.indexOf(file) !== -1) return;
+    for (var i = 0; i < ignore_folders.length; i++) {
+        if (file.match(ignore_folders[i])) return;
     }
     process.stdout.write('<url>\n');
     process.stdout.write('<loc>');
